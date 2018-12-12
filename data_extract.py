10702ee8f4ae252c886518f3ce0c12b39ea4f917
Matthew <matthewbropez@gmail.com>
2018-12-11 21:04:27 -0500

    Modified data_extract.py to use the animal crossing wiki instead.

--- data_extract.py
+++ data_extract.py
@@ -2,22 +2,29 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import csv
 
-html = urlopen('https://www.thonky.com/animal-crossing-new-leaf/paintings-works-of-art')
+html = urlopen('https://animalcrossing.fandom.com/wiki/Forgery')
 soup = BeautifulSoup(html, "html.parser")
-table = soup.find("table", {"class": "table table-bordered"})
+table_painting = soup.find_all("table", {"class": "wikitable"})[0]
+table_statue = soup.find_all("table", {"class": "wikitable"})
 
-starter_link = "https://www.thonky.com/animal-crossing-new-leaf/"
 rows = []
 
 # this is to set all of the img tag's sources into it's td instead
 # it also adds all of the information to the rows list
-for tr in table.find_all("tr"):
+for tr in table_painting.find_all("tr"):
     for td in tr.find_all("td"):
         if td.img:
-            td.string = starter_link + td.img['src']
+            td.string = td.img['src']
 
-    rows.append(tr)
+for tr in table_statue.find_all("tr"):
+    for td in tr.find_all("td"):
+        if td.img:
+            td.string = td.img['src']
 
-with open('output_file.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerows(rows)
+    # rows.append(tr)
+
+# TODO: Need to find out how to transfer these two tables above into one and into a formatted csv
+
+# with open('output_file.csv', 'w') as f:
+#     writer = csv.writer(f)
+#     writer.writerows(rows)
